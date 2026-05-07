5e681e9d04ab04a7a825e8405564ac4b0f79f598
Marius Thomsen <mariu@itu.dk>
2026-05-07 20:01:19 +0200

    utils

--- cv_utils.py
+++ cv_utils.py
@@ -1,30 +1,3 @@
-# Our first set of imports will all be our backbone library for all of our models, Torch
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset,DataLoader
-
-# While torch takes care of most of the data-loading, torch vision will be what we need for the computer "vision" part of this tutorial
-import torchvision
-from torchvision.transforms import v2
-from torchvision.models import resnet50, ResNet50_Weights
-import torchvision.models as models
-#from torchmetrics.classification import MulticlassAccuracy
-
-#This will be our helper functions for video reading and other general "math" functions
-import cv2
-import numpy as np
-import math
-import os
-#import h5py
-
-# Finally, our plotting library
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-
-
 def seconds_to_mmss(seconds):
     """Convert seconds to MM:SS format."""
     seconds = int(round(seconds))
@@ -36,3 +9,23 @@ def mmss_to_seconds(time_str):
     """Convert MM:SS format to seconds."""
     minutes, seconds = time_str.strip().split(":")
     return int(minutes) * 60 + int(seconds)
+
+def mmss_to_seconds_flexible(time_str):
+    """
+    Convert M:SS or MM:SS to seconds.
+    """
+    time_str = clean_timestamp(time_str)
+    minutes, seconds = time_str.strip().split(":")
+    return int(minutes) * 60 + int(seconds)
+
+
+def clean_timestamp(time_str):
+    """
+    Clean timestamp mistakes such as:
+    MM:00:20 -> 00:20
+    00:20 -> 00:20
+    """
+    time_str = time_str.strip()
+    time_str = time_str.replace("MM:", "")
+    time_str = time_str.replace("SS", "")
+    return time_str
